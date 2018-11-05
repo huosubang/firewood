881ffb661792f34c26fdd23c7e604858ec522552
xutao <xuyz1988@163.com>
2018-11-05 11:30:15 +0800

    fix update db

--- server.go
+++ server.go
@@ -13,6 +13,9 @@ type Server struct {
 func RunServer(addr string, handler http.Handler) error {
 	n := negroni.New()
 	n.Use(NewLoggerHandler())
+	//recovery := negroni.NewRecovery()
+	//recovery.PanicHandlerFunc = func(d *negroni.PanicInformation){fmt.Println("----paic")}
+	//n.Use(recovery)
 	n.UseHandler(handler)
 	err := http.ListenAndServe(addr, n)
 	return err
